cfacc9df27c9804f556e9be6c9f9bb78cc91686b
Himujjal <himu@tuta.io>
2021-03-28 22:57:41 +0530

    added build.sh instead of build.zig

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+rm -rf lib
+
 TSV=0.19.2
 wget https://github.com/tree-sitter/tree-sitter/archive/v$TSV.tar.gz -O yo.tar.gz
 tar -xf yo.tar.gz
-zig translate-c -lc tree-sitter-$TSV/lib/include/tree_sitter/api.h > src/api.zig
+zig translate-c -lc tree-sitter-$TSV/lib/include/tree_sitter/api.h > src/main.zig
 mv tree-sitter-$TSV/lib .
 
 rm -rf tree-sitter-$TSV
